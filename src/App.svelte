af88c8735fba7468cb54f44f3272dab991f4fbe0
Jonathan Talcott <jetalcott@milcorp.com>
2024-04-02 11:04:26 -0400

    Use #app container as container instead of page

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,24 @@
 	import DisconnectedDialog from "$lib/sws/components/DisconnectedDialog.svelte";
 </script>
 
-<DisconnectedDialog></DisconnectedDialog>
-<header>Header</header>
-<main>Main</main>
-<footer>Footer</footer>
+<div>
+	<DisconnectedDialog></DisconnectedDialog>
+	<header>Header</header>
+	<main>Main</main>
+	<footer>Footer</footer>
+</div>
+
+<style>
+	div {
+		width: 100%;
+		height: 100%;
+
+		flex-direction: column;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		background-color: #ddd;
+		color: #000;
+	}
+</style>
