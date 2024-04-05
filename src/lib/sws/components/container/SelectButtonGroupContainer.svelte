ed07d0ce526b2874f521a81297fc22266199b5e8
Jonathan Talcott <jetalcott@milcorp.com>
2024-04-05 09:40:52 -0400

    Better store names

--- src/lib/sws/components/container/SelectButtonGroupContainer.svelte
+++ src/lib/sws/components/container/SelectButtonGroupContainer.svelte
@@ -27,25 +27,20 @@
      */
     let selected = numbers.get(`${id}.selected`);
 
-    let visibles: Readable<boolean[]>;
-    let labels: Readable<string[]>;
+    let visibleArray: Readable<boolean[]>;
+    let labelArray: Readable<string[]>;
 
     $: {
         console.warn("!!! Recalculating array stores !!!");
-        visibles = derived(Array.from(Array($count).keys(), (i) => booleans.get(`${id}.visibles[${i}]`)), value => value);
-        labels = derived(Array.from(Array($count).keys(), (i) => strings.get(`${id}.labels[${i}]`)), value => value);
+        visibleArray = derived(Array.from(Array($count).keys(), (i) => booleans.get(`${id}[${i}].visible`)), value => value);
+        labelArray = derived(Array.from(Array($count).keys(), (i) => strings.get(`${id}[${i}].label`)), value => value);
     }
-
-    // DEBUG
-    // $count = 3;
-    // $selected = 1;
-	// $visible = "0,1,2";
 </script>
 
 <span class={clazz}>
     {#each { length: $count } as _, index}
-        {#if $visibles[index]}
-            <button class={memberClass} class:selected={$selected == index} on:click={() => $selected = index}>{$labels[index]}</button>
+        {#if $visibleArray[index]}
+            <button class={memberClass} class:selected={$selected == index} on:click={() => $selected = index}>{$labelArray[index]}</button>
         {/if}
     {/each}
 </span>
